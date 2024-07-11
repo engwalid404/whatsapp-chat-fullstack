6d5e711f0b33581652175e15aa0089e098bf15e0
Walid Rabie <you@example.com>
2024-07-11 21:24:38 +0300

    chat application

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+	providers: [
+	  {
+		domain: "https://charmed-anemone-6.clerk.accounts.dev",
+		applicationID: "convex",
+	  },
+	]
+  };
+
+
+export default authConfig ;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import ConvexClientProvider from "@/providers/convex-client-provider";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+      <ThemeProvider   attribute="class"   defaultTheme="system"  enableSystem  disableTransitionOnChange >
+      <ConvexClientProvider>
+						{children}
+						<Toaster />
+					</ConvexClientProvider>
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
